da33feea086a04672de8e0ca91d4a6c9ca7bbef8
isabellefrancis <belle1306@gmail.com>
2020-12-14 16:11:21 +0800

    stuckatstate

--- src/App.js
+++ src/App.js
@@ -10,19 +10,42 @@ class App extends React.Component {
       weather: null,
       error: false
     };
+    this.handleInput = this.handleInput.bind(this);
+    this.getWeather = this.getWeather.bind(this);
   }
 
-  handleInput() {
+  handleInput(event) {
     // handle form input here
+    this.setState({ value: event.target.value });
   }
 
-  getWeather() {
+  async getWeather(location) {
     // call Open Weather API
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+    );
+    return result.json();
   }
+}
+//alert('Get Weather: '+ this.state.value);
+//event.preventDefault();
 
-  render() {
-    return <div>Hello World!</div>;
-  }
+render();
+{
+  return (
+    <form onSubmit={this.handleInput}>
+      <label>
+        Get Location Weather:
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleInput}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    //<div><LoadingIndicator is loading={isLoading}/></div>
+  );
 }
 
 export default App;
